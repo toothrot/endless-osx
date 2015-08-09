94c2419dc3612f3c40ede6b4cb6a676453d2fe24
Alex Rakoczy <scissorjammer@gmail.com>
2015-08-09 12:01:39 -0400

    Initial Commit

---
+++ endless/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  endless
+//
+//  Created by Alexander Rakoczy on 8/9/15.
+//  Copyright (c) 2015 Alexander Rakoczy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
